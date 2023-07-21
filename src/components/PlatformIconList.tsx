da9062693b23308df9aab9362403398bce798e13
DRAIA Oussama <100196763+Droronoaa@users.noreply.github.com>
2023-07-21 10:55:03 +0100

    Displaying platform icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -20,7 +20,7 @@ interface Props {
 const PlatformIconList = ({ platforms }: Props) => {
     const iconMap: {[key: string]: IconType} = {
         pc: FaWindows,
-        playstationL: FaPlaystation,
+        playstation: FaPlaystation,
         xbox: FaXbox,
         nintendo: SiNintendo,
         mac: FaApple,
